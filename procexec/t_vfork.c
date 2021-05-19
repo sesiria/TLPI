6b7c4e8ae7f12e4594830e11f054d3ef0c67adfe
sesiria <stjohnson_free@hotmail.com>
2021-05-19 14:52:22 +0800

    add procexec

---
+++ procexec/t_vfork.c
@@ -0,0 +1,21 @@
+#include "tlpi_hdr.h"
+
+int main(int argc, char*argv[])
+{
+    int istack = 222;
+
+    switch(vfork()) {
+    case -1: 
+        errExit("vfork");
+    case 0:                 /* Child executes first, in parent's memory space */
+        sleep(3);
+
+        write(STDOUT_FILENO, "Child executing\n", 16);
+        istack *= 3;
+        _exit(EXIT_SUCCESS);
+    default:                /* Parent is blocked until child exits */
+        write(STDOUT_FILENO, "Parent executing\n", 17);
+        printf("istack=%d\n", istack);
+        exit(EXIT_SUCCESS);
+    }
+}
