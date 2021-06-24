eb2a0053f072563e1d178cb2c24b3a9e94538431
sesiria <stjohnson_free@hotmail.com>
2021-06-24 18:02:25 +0800

    add rlimit implementation

---
+++ procres/print_rlimit.h
@@ -0,0 +1,10 @@
+/* print_rlimit.h
+
+   Header file for print_rlimit.c.
+*/
+#ifndef PRINT_RLIMIT_H      /* Prevent accidental double inclusion */
+#define PRINT_RLIMIT_H
+
+int printRlimit(const char *msg, int resource);
+
+#endif
---
+++ procres/rlimit_nproc.c
@@ -0,0 +1,14 @@
+/*
+* Setting the RLIMIT_NPROC resource limit
+* File Name: rlimit_nproc.c
+* author: sesiria 2021-06-24
+* Souce from the text [Kerrisk, Micheal, 2010, The linux programming interface]
+*/
+#include <sys/resource.h>
+#include "print_rlimit.h"               /* Declaration of printRlimit() */
+#include "tlpi_hdr.h"
+
+int main(int argc, char * argv[])
+{
+    
+}
